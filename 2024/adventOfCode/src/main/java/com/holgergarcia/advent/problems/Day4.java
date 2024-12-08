01139c447bf32c244fb173e79700ad2713b34fe9
Holger Garcia Fernandez <holger@holgergarcia.com>
2024-12-08 01:02:27 +0000

    advent of code day 4

--- 2024/adventOfCode/src/main/java/com/holgergarcia/advent/problems/Day4.java
+++ 2024/adventOfCode/src/main/java/com/holgergarcia/advent/problems/Day4.java
@@ -6,7 +6,7 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Day4 {
+public class Day4 { 
 
     public static void main(String[] args) throws IOException {
         List<String> inputs = Files.readAllLines(Path.of("src/main/resources/inputs/2024/day4Input"));
