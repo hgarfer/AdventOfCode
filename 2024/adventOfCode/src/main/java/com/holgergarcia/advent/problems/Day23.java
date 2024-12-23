09c6dba9dc365966f282ec5998b0ae97d41369d9
Holger Garcia Fernandez <holger@holgergarcia.com>
2024-12-23 19:54:13 +0000

    added placeholders for remaining problems

---
+++ 2024/adventOfCode/src/main/java/com/holgergarcia/advent/problems/Day23.java
@@ -0,0 +1,28 @@
+package com.holgergarcia.advent.problems;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Day23 {
+
+    public static void main(String[] args) throws IOException {
+        List<String> inputs = Files.readAllLines(Path.of("src/main/resources/inputs/2024/day23Input"));
+
+        System.out.println("day 23 step 1:" + solveStep1(inputs));
+        System.out.println("day 23 step 2:" + solveStep2(inputs));
+    }
+
+    static int solveStep1(List<String> inputs) {
+        int result = 0;
+
+        return result;
+    }
+
+    static int solveStep2(List<String> inputs) {
+        int result = 0;
+
+        return result;
+    }
+}
